a8f23684136a519832819e482793f8dceecf02bd
pmewbourne20 <43067160+pmewbourne20@users.noreply.github.com>
2018-11-16 14:28:17 -0500

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,5 +1,9 @@
+
+
 from PIL import Image
 import system
+from PIL import ImageFont
+from PIL import ImageDraw
 image = sys.argv[1]
 
 img = Image.open(image)
@@ -8,3 +12,15 @@ imgx, imgy = img.size
 
 sixthx = int(imgx/6)
 sixthy = int(imgy/6)
+
+img2 = Image.open("frog.jpeg")
+
+img2 = img2.resize((sixthx,sixthy), Image.ANTIALIAS)
+
+img.paste(img2, ((imgx-2*sixthx),(imgy-2*sixthy)))
+
+draw = ImageDraw.Draw(img)
+font = ImageFont.truetype("papyrus.ttf", 16)
+draw.text((sixthx,sixthy),"Graphic Design is my Passion",(255,0,0),font=font)
+
+img.save("new-image.jpg")
